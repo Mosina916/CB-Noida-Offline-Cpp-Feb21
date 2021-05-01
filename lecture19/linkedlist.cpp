e279f9eb5ef121fc98d751bda1aefd9484a9ead3
mosina916 <mosinaashraf1913@gmail.com>
2021-05-01 14:35:07 +0530

    linkedlist

--- lecture19/linkedlist.cpp
+++ lecture19/linkedlist.cpp
@@ -34,6 +34,57 @@ void insertatfront(node*&head,node*&tail,int data){
 
 
 }
+void insertattail(node*&head,node*&tail,int data){
+	// pehle node banega jisko point n karega
+
+	node*n=new node(data);
+	// 1. ll is empty
+	if(head==NULL){
+		head=tail=n; //pehla node create hoga ll ka
+
+
+	}
+	// 2. ll is not empty
+	else{
+		tail->next=n;
+		tail=n;
+
+	}
+
+
+}
+int length(node*head){
+	int cnt=0;
+
+	while(head!=NULL){
+		cnt++;
+		head=head->next;
+	}
+	return cnt;
+
+}
+
+void insertatanypostion(node*&head,node*&tail,int data,int pos){
+	if(pos==0){
+		insertatfront(head,tail,data);
+	}
+	if(pos>=length(head)){
+		insertattail(head,tail,data);
+	}
+	else{
+		node*temp=head;
+		node*n=new node(data);
+		for(int jump=1;jump<pos;jump++){
+			temp=temp->next;
+		}
+		n->next=temp->next;
+		temp->next=n;
+
+	}
+
+
+}
+
 void print(node*head){
 	// node*temp=head;
 	// while(temp!=NULL){
@@ -51,6 +102,7 @@ void print(node*head){
 }
 
 
+
 int main(){
 	// node n1(40); //n1 is object of type node
 	// node n2(50);
@@ -66,9 +118,16 @@ int main(){
 	insertatfront(head,tail,12);
 	insertatfront(head,tail,16);
 	insertatfront(head,tail,18);
-	insertatfront(head,tail,20);
+	insertatfront(head,tail,20); //20 18 16 12 9
+
 	print(head);
+	insertattail(head,tail,19);//20 18 16 12 9 19
+	insertattail(head,tail,190);//20 18 16 12 9 19 190
 	print(head);
+	insertatanypostion(head,tail,50,3);//20 18 16 50 12 9 19 190
+	print(head);
+
+
 
 
 	return 0;
